a342df29ab8fc8983bd2018a11c269680bf5f123
Christian Lorenzo <clbmiami2004@gmail.com>
2021-02-16 12:23:44 -0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,60 @@
+// swift-tools-version:5.3
+
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+
+import PackageDescription
+
+let package = Package(
+  name: "CalendarControl",
+    
+    //This code indicates what platforms it can run:
+  platforms: [
+//1
+    .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
+  ],
+    
+    // These are the products the package provides. These can be either a library-code you cn import into other Swift projects-or an executable-code you can run by the operating system. A product is a target you can export for other packages to use.
+  products: [
+//2
+    .library(name: "CalendarControl",
+             targets: ["CalendarControl"])
+  ],
+    
+    // Targets are modules of code that are built independently. Here you add a path for your XCFramework
+  targets: [
+//3
+    .binaryTarget(name: "CalendarControl",
+                  path: "./Sources/CalendarControl.xcframework")
+  ]
+)
